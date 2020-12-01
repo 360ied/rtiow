1d7247b10fd6cceb523d29ad68ad0c8b21aff9de
rian <19516527+360ied@users.noreply.github.com>
2020-12-01 11:38:22 -0500

    allow positioning and orienting the camera

--- camera/camera.go
+++ camera/camera.go
@@ -9,11 +9,6 @@ import (
 )
 
 type Camera struct {
-	AspectRatio    float64
-	ViewportHeight float64
-	ViewportWidth  float64
-	FocalLength    float64
-
 	Origin          vec3.Point3
 	Horizontal      vec3.Vec3
 	Vertical        vec3.Vec3
@@ -21,45 +16,35 @@ type Camera struct {
 }
 
 func NewCamera(
-	// aspectRatio, viewportHeight, focalLength float64,
-	// origin vec3.Point3,
-	// horizontal, vertical vec3.Vec3,
+	lookFrom, lookAt vec3.Point3,
+	vUp vec3.Vec3,
 	vFov, aspectRatio float64,
-) Camera {
+) (cam Camera) {
 	theta := helpers.DegreesToRadians(vFov)
 	h := math.Tan(theta / 2.0)
 	viewportHeight := 2.0 * h
 	viewportWidth := aspectRatio * viewportHeight
 
-	const focalLength = 1.0
+	w := lookFrom.SubtractVec3(lookAt).UnitVector()
+	u := vUp.Cross(w).UnitVector()
+	v := w.Cross(u)
 
-	origin := vec3.Point3{} // 0, 0, 0
-	horizontal := vec3.Vec3{X: viewportWidth}
-	vertical := vec3.Vec3{Y: viewportHeight}
-
-	return Camera{
-		AspectRatio:    aspectRatio,
-		ViewportHeight: viewportHeight,
-		ViewportWidth:  viewportWidth,
-		FocalLength:    focalLength,
-		Origin:         origin,
-		Horizontal:     horizontal,
-		Vertical:       vertical,
-		LowerLeftCorner: origin.SubtractVec3(
-			horizontal.DivideFloat(2.0),
-		).SubtractVec3(
-			vertical.DivideFloat(2.0),
-		).SubtractVec3(vec3.Vec3{Z: focalLength}), // 0, 0, focalLength,
-	}
+	cam.Origin = lookFrom
+	cam.Horizontal = u.MultiplyFloat(viewportWidth)
+	cam.Vertical = v.MultiplyFloat(viewportHeight)
+	cam.LowerLeftCorner = cam.Origin.
+		SubtractVec3(cam.Horizontal.DivideFloat(2.0)).
+		SubtractVec3(cam.Vertical.DivideFloat(2.0)).
+		SubtractVec3(w)
+	return
 }
 
-func (c Camera) Ray(u, v float64) material.Ray {
+func (c Camera) Ray(s, t float64) material.Ray {
 	return material.Ray{
 		Origin: c.Origin,
-		Direction: c.LowerLeftCorner.AddVec3(
-			c.Horizontal.MultiplyFloat(u),
-		).AddVec3(
-			c.Vertical.MultiplyFloat(v),
-		).SubtractVec3(c.Origin),
+		Direction: c.LowerLeftCorner.
+			AddVec3(c.Horizontal.MultiplyFloat(s)).
+			AddVec3(c.Vertical.MultiplyFloat(t)).
+			SubtractVec3(c.Origin),
 	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"math/rand"
 	"os"
 	"sync"
@@ -10,41 +9,44 @@ import (
 	"rtiow/camera"
 	"rtiow/colour"
 	"rtiow/material"
+	"rtiow/material/dielectric"
 	"rtiow/material/lambertian"
+	"rtiow/material/metal"
 	"rtiow/sphere"
 	"rtiow/vec3"
 )
 
+//goland:noinspection GoStructInitializationWithoutFieldNames
 func main() {
 	// Image
 	const (
 		aspectRatio     = 16.0 / 9.0
-		imageWidth      = 400
+		imageWidth      = 1920
 		imageHeight     = imageWidth / aspectRatio
-		samplesPerPixel = 25
-		maxDepth        = 10
+		samplesPerPixel = 100
+		maxDepth        = 50
 	)
 
 	// World
-	r := math.Cos(math.Pi / 4)
+	matGround := lambertian.Lambertian{vec3.Colour{0.8, 0.8, 0.0}}
+	matCenter := lambertian.Lambertian{vec3.Colour{0.1, 0.2, 0.5}}
+	matLeft := dielectric.Dielectric{1.5}
+	matRight := metal.Metal{vec3.Colour{0.8, 0.6, 0.2}, 0.0}
 
-	world := material.HittableList{
-		Objects: []material.Hittable{
-			sphere.Sphere{
-				Center: vec3.Point3{X: -r, Z: -1},
-				Radius: r,
-				Mat:    lambertian.Lambertian{Albedo: vec3.Colour{Z: 1}},
-			},
-			sphere.Sphere{
-				Center: vec3.Point3{X: r, Z: -1},
-				Radius: r,
-				Mat:    lambertian.Lambertian{Albedo: vec3.Colour{X: 1}},
-			},
-		},
-	}
+	world := material.HittableList{}
+	// ground
+	world.Add(sphere.Sphere{vec3.Point3{0.0, -100.5, -1.0}, 100.0, matGround})
+	// blue ball in the center
+	world.Add(sphere.Sphere{vec3.Point3{0.0, 0.0, -1.0}, 0.5, matCenter})
+	// glass ball on the left
+	world.Add(sphere.Sphere{vec3.Point3{-1.0, 0.0, -1.0}, 0.5, matLeft})
+	// slightly smaller glass ball within the glass ball on the left
+	world.Add(sphere.Sphere{vec3.Point3{-1.0, 0.0, -1.0}, -0.45, matLeft})
+	// yellow metal ball on the right
+	world.Add(sphere.Sphere{vec3.Point3{1.0, 0.0, -1.0}, 0.5, matRight})
 
 	// Camera
-	cam := camera.NewCamera(90.0, aspectRatio)
+	cam := camera.NewCamera(vec3.Point3{-2, 2, 1}, vec3.Point3{0, 0, -1}, vec3.Vec3{0, 1, 0}, 90.0, aspectRatio)
 
 	wg := new(sync.WaitGroup)
 
